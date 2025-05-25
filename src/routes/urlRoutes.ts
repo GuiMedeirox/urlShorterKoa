93e8ff031d5eb68c846160ee35fd5b816839e05a
Guilherme <guimedeiroxdev@gmail.com>
2025-05-25 00:05:40 -0300

    reset repo

--- src/routes/urlRoutes.ts
+++
@@ -1,9 +0,0 @@
-import Router from 'koa-router';
-import { getUrl, shortenUrl } from '../controllers/urlController';
-
-const router = new Router();
-
-router.post('/shorten', shortenUrl);
-router.get('/:shortCode', getUrl);
-
-export default router;
